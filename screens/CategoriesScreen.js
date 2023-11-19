bb5bc043231b4e9e958e8b5721223d61b6a588d9
ANS <36783010+answebdev@users.noreply.github.com>
2023-11-19 10:24:59 -0800

    add shell screens

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,26 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+
+import { CATEGORIES } from '../data/dummy-data';
+
+// Optional:
+// We can also do this down in the JSX in the FlatList,
+// but we're doing this here to keep it lean.
+function renderCategoryITem(itemData) {
+    return (
+        <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+    );
+}
+
+// function CategoriesScreen() {
+const CategoriesScreen = () => {
+    return (
+        <FlatList
+            data={CATEGORIES}
+            keyExtractor={(item) => item.id}
+            renderItem={renderCategoryITem}
+        />
+    );
+};
+
+export default CategoriesScreen;
