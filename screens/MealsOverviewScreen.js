4526fb22d8e97102dcfb9976b97498b5ff671842
ANS <36783010+answebdev@users.noreply.github.com>
2023-11-21 18:14:13 -0800

    display meal text

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,12 +1,24 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }) {
     const catId = route.params.categoryId;
 
+    // Return 'true' for all the meals that belong to this category, i.e.,
+    // Return 'true' if we want to keep a meal; false if we don't:
+    const displayedMeals = MEALS.filter((mealItem) => {
+        // This will give us our displayed meals:
+        return mealItem.categoryIds.indexOf(catId) >= 0;
+    });
+
+    function renderMealItem(itemData) {
+        return <MealItem title={itemData.item.title} />;
+    }
+
     return (
         <View style={styles.container}>
-            <Text>Meals Overview Screen - {catId}</Text>
+            <FlatList data={displayedMeals} keyExtractor={(item) => item.id} renderItem={renderMealItem} />
         </View>
     );
 }
