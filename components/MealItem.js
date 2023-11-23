8e1aba052692460cb0172f229c572fef8f595bd6
ANS <36783010+answebdev@users.noreply.github.com>
2023-11-23 13:11:48 -0800

    add details to screen

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,5 +1,6 @@
 import { View, Text, Pressable, Image, StyleSheet, Platform } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 function MealItem({ id, title, imageUrl, duration, complexity, affordability }) {
     const navigation = useNavigation();
@@ -26,11 +27,11 @@ function MealItem({ id, title, imageUrl, duration, complexity, affordability }) 
                         <Image style={styles.image} source={{ uri: imageUrl }} />
                         <Text style={styles.title}>{title}</Text>
                     </View>
-                    <View style={styles.details}>
-                        <Text style={styles.detailItem}>{duration} min.</Text>
-                        <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-                        <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-                    </View>
+                    <MealDetails
+                        duration={duration}
+                        complexity={complexity}
+                        affordability={affordability}
+                    />
                 </View>
             </Pressable>
         </View>
@@ -71,15 +72,5 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         margin: 8
-    },
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 8
-    },
-    detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12
     }
 });
--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,10 +1,30 @@
-import { Text } from "react-native";
+import { View, Text, Image } from "react-native";
+import MealDetails from "../components/MealDetails";
+import { MEALS } from "../data/dummy-data";
 
 function MealDetailsScreen({ route }) {
     const mealId = route.params.mealId;
 
+    const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
     return (
-        <Text>This is the Meal Details Screen for {mealId}</Text>
+        <View>
+            <Image source={{ uri: selectedMeal.imageUrl }} />
+            <Text>{selectedMeal.title}</Text>
+            <MealDetails
+                duration={selectedMeal.duration}
+                complexity={selectedMeal.complexity}
+                affordability={selectedMeal.affordability}
+            />
+            <Text>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => (
+                <Text key={ingredient}>{ingredient}</Text>
+            ))}
+            <Text>Steps</Text>
+            {selectedMeal.steps.map(step => (
+                <Text key={step}>{step}</Text>
+            ))}
+        </View>
     );
 }
 
