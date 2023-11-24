d24ffc37f04c693ebc6702d776b1b74fe0bfb1c0
ANS <36783010+answebdev@users.noreply.github.com>
2023-11-23 16:36:44 -0800

    style details screen

--- components/MealDetails.js
+++ components/MealDetails.js
@@ -1,12 +1,12 @@
 import { Text, View, StyleSheet } from "react-native";
 
-function MealDetails({ duration, complexity, affordability }) {
+function MealDetails({ duration, complexity, affordability, style, textStyle }) {
     return (
-        <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration} min.</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-        </View>
+        <View style={[styles.details, style]}>
+            <Text style={[styles.detailItem, textStyle]}>{duration} min.</Text>
+            <Text style={[styles.detailItem, textStyle]}>{complexity.toUpperCase()}</Text>
+            <Text style={[styles.detailItem, textStyle]}>{affordability.toUpperCase()}</Text>
+        </View >
     );
 }
 
@@ -17,10 +17,10 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'center',
-        padding: 8
+        padding: 8,
     },
     detailItem: {
         marginHorizontal: 4,
-        fontSize: 12
+        fontSize: 12,
     }
 });
