cd394cd28f0ec2d3ef608a44dc2cbc11d031ada3
ANS <36783010+answebdev@users.noreply.github.com>
2023-12-18 19:27:09 -0800

    add header icon button

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,13 +1,31 @@
+import { useLayoutEffect } from "react";
 import { View, Text, Image, StyleSheet, ScrollView } from "react-native";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetails/Subtitle";
 import List from "../components/MealDetails/List";
 import { MEALS } from "../data/dummy-data";
+import IconButton from "../components/IconButton";
 
-function MealDetailsScreen({ route }) {
+function MealDetailsScreen({ route, navigation }) {
     const mealId = route.params.mealId;
 
     const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+    function headerButtonPressHandler() {
+        console.log("PRESSED");
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => {
+                return <IconButton
+                    icon='star'
+                    color='white'
+                    onPress={headerButtonPressHandler}
+                />;
+            }
+        });
+    }, [navigation, headerButtonPressHandler]);
 
     return (
         <ScrollView style={styles.rootContainer}>
